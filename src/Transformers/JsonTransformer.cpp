3bf7d514ec94981c898ea59b9e5a441abd628c73
Geo <tp53.geo@gmail.com>
2025-01-30 01:50:52 +0100

    change JsonTransformer  to use dynamic allocator

--- src/Transformers/JsonTransformer.cpp
+++ src/Transformers/JsonTransformer.cpp
@@ -1,25 +1,20 @@
 #include "JsonTransformer.h"
-#include <ArduinoJson.h>
 #include <stdexcept>
 
 std::string JsonTransformer::emptyJsonStructure() {
-    // Créer un document JSON avec la structure vide
-    DynamicJsonDocument doc(256); // Taille ajustable selon vos besoins
+    JsonDocument doc;
 
-    // Ajouter les tableaux vides pour "entries" et "categories"
     JsonObject root = doc.to<JsonObject>();
     root.createNestedArray("entries");
     root.createNestedArray("categories");
 
-    // Convertir le document JSON en une chaîne
     std::string jsonContent;
     serializeJson(doc, jsonContent);
-
     return jsonContent;
 }
 
 std::string JsonTransformer::toJson(const std::vector<Entry>& entries) {
-    DynamicJsonDocument doc(8192); // Taille ajustable en fonction des besoins
+    JsonDocument doc;
 
     JsonArray entriesArray = doc.to<JsonArray>();
     for (const auto& entry : entries) {
@@ -41,27 +36,24 @@ std::string JsonTransformer::toJson(const std::vector<Entry>& entries) {
 }
 
 std::vector<Entry> JsonTransformer::fromJsonToEntries(const std::string& jsonContent) {
-    DynamicJsonDocument doc(8192); // Taille ajustable
+    JsonDocument doc;
 
-    DeserializationError error = deserializeJson(doc, jsonContent);
+    auto error = deserializeJson(doc, jsonContent);
     if (error) {
         throw std::runtime_error("Failed to parse JSON: " + std::string(error.c_str()));
     }
 
     std::vector<Entry> entries;
-
     JsonArray entriesArray = doc["entries"].as<JsonArray>();
+
     for (JsonObject entryObj : entriesArray) {
         Entry entry;
         entry.setId(entryObj["id"].as<std::string>());
         entry.setServiceName(entryObj["serviceName"].as<std::string>());
         entry.setUsername(entryObj["username"].as<std::string>());
-        entry.setPassword(entryObj["password"].as<std::string>());
+        entry.setPassword(entryObj["encryptedPassword"].as<std::string>());
         entry.setCategoryIndex(entryObj["categoryIndex"].as<size_t>());
         entry.setNotes(entryObj["notes"].as<std::string>());
-        entry.setNotes2(entryObj["notes2"].as<std::string>());
-        entry.setNotes3(entryObj["notes3"].as<std::string>());
-        entry.setLink(entryObj["link"].as<std::string>());
         entry.setCreatedAt(entryObj["createdAt"].as<long>());
         entry.setUpdatedAt(entryObj["updatedAt"].as<long>());
         entry.setExpiresAt(entryObj["expiresAt"].as<long>());
@@ -73,7 +65,7 @@ std::vector<Entry> JsonTransformer::fromJsonToEntries(const std::string& jsonCon
 }
 
 std::string JsonTransformer::toJson(const std::vector<Category>& categories) {
-    DynamicJsonDocument doc(4096); // Taille ajustable
+    JsonDocument doc;
 
     JsonArray categoriesArray = doc.to<JsonArray>();
     for (const auto& category : categories) {
@@ -90,16 +82,16 @@ std::string JsonTransformer::toJson(const std::vector<Category>& categories) {
 }
 
 std::vector<Category> JsonTransformer::fromJsonToCategories(const std::string& jsonContent) {
-    DynamicJsonDocument doc(4096); // Taille ajustable
+    JsonDocument doc;
 
-    DeserializationError error = deserializeJson(doc, jsonContent);
+    auto error = deserializeJson(doc, jsonContent);
     if (error) {
         throw std::runtime_error("Failed to parse JSON: " + std::string(error.c_str()));
     }
 
     std::vector<Category> categories;
-
     JsonArray categoriesArray = doc["categories"].as<JsonArray>();
+
     for (JsonObject categoryObj : categoriesArray) {
         Category category;
         category.setIndex(categoryObj["index"].as<size_t>());
@@ -114,12 +106,9 @@ std::vector<Category> JsonTransformer::fromJsonToCategories(const std::string& j
 }
 
 std::string JsonTransformer::mergeEntriesAndCategoriesToJson(const std::vector<Entry>& entries, const std::vector<Category>& categories) {
-    DynamicJsonDocument doc(14000); // Taille ajustable selon vos besoins
+    JsonDocument doc;
 
-    // Créer un objet JSON principal
     JsonObject root = doc.to<JsonObject>();
-
-    // Ajouter les catégories
     JsonArray categoriesArray = root.createNestedArray("categories");
     for (const auto& category : categories) {
         JsonObject categoryObj = categoriesArray.createNestedObject();
@@ -129,27 +118,21 @@ std::string JsonTransformer::mergeEntriesAndCategoriesToJson(const std::vector<E
         categoryObj["iconPath"] = category.getIconPath();
     }
 
-    // Ajouter les entrées
     JsonArray entriesArray = root.createNestedArray("entries");
     for (const auto& entry : entries) {
         JsonObject entryObj = entriesArray.createNestedObject();
         entryObj["id"] = entry.getId();
         entryObj["serviceName"] = entry.getServiceName();
         entryObj["username"] = entry.getUsername();
-        entryObj["password"] = entry.getPassword();
+        entryObj["encryptedPassword"] = entry.getPassword();
         entryObj["categoryIndex"] = entry.getCategoryIndex();
         entryObj["notes"] = entry.getNotes();
-        entryObj["notes2"] = entry.getNotes2();
-        entryObj["notes3"] = entry.getNotes3();
-        entryObj["link"] = entry.getLink();
         entryObj["createdAt"] = static_cast<long>(entry.getCreatedAt());
         entryObj["updatedAt"] = static_cast<long>(entry.getUpdatedAt());
         entryObj["expiresAt"] = static_cast<long>(entry.getExpiresAt());
     }
 
-    // Convertir le document JSON en une chaîne
     std::string jsonContent;
     serializeJson(doc, jsonContent);
-
     return jsonContent;
 }
--- src/Transformers/JsonTransformer.h
+++ src/Transformers/JsonTransformer.h
@@ -1,6 +1,7 @@
 #ifndef JSON_TRANSFORMER_H
 #define JSON_TRANSFORMER_H
 
+#include <ArduinoJson.h>
 #include <string>
 #include <vector>
 #include <Models/Category.h>
@@ -11,19 +12,10 @@ class JsonTransformer {
 public:
     std::string emptyJsonStructure();
 
-    // Convertit un std::vector<Entry> en chaîne JSON
     std::string toJson(const std::vector<Entry>& vault);
-
-    // Parse une chaîne JSON pour obtenir un std::vector<Entry>
     std::vector<Entry> fromJsonToEntries(const std::string& jsonContent);
-
-    // Convertit un vecteur de catégories en chaîne JSON
     std::string toJson(const std::vector<Category>& categories);
-
-    // Parse une chaîne JSON pour obtenir un vecteur de catégories
     std::vector<Category> fromJsonToCategories(const std::string& jsonContent);
-
-    // Fusionne un vecteur d'entries et un vecteur de catégories en un seul JSON valide
     std::string mergeEntriesAndCategoriesToJson(const std::vector<Entry>& entries, const std::vector<Category>& categories);
 };
 
