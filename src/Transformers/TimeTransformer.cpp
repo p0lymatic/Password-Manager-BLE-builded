647b02204c4e40f8c6be79d2a300c280bbcfebcd
Geo <tp53.geo@gmail.com>
2025-01-31 05:25:51 +0100

    more timing choice for vault/screen lock

--- src/Transformers/TimeTransformer.cpp
+++ src/Transformers/TimeTransformer.cpp
@@ -1,14 +1,16 @@
 #include "TimeTransformer.h"
 
 TimeTransformer::TimeTransformer() {
-    timeLabels = {"1 minute", "3 minutes", "5 minutes", "10 minutes", "30 minutes", "1 hour"};
+    timeLabels = {"30 secondes", "1 minute", "3 minutes", "5 minutes", "10 minutes", "30 minutes", "1 hour", "5 hours"};
     timeValues = {
+        30 * 1000,       // 30 sec = 30 000 ms
         1 * 60 * 1000,   // 1 minute  = 60 000 ms
         3 * 60 * 1000,   // 3 minutes = 180 000 ms
         5 * 60 * 1000,   // 5 minutes = 300 000 ms
         10 * 60 * 1000,  // 10 minutes = 600 000 ms
         30 * 60 * 1000,  // 30 minutes = 1 800 000 ms
-        1 * 60 * 60 * 1000 // 1 heure = 3 600 000 ms
+        1 * 60 * 60 * 1000, // 1 heure = 3 600 000 ms
+        5 * 60 * 60 * 1000 // 5 heures
     };
 }
 
